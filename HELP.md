082dc40a076de2b42bd280cea8643490061d99dc
Hossam Hammady <github@hammady.net>
2022-02-20 23:11:13 -0500

    Add stream/record start/stop/threshold actions

--- HELP.md
+++ HELP.md
@@ -3,16 +3,33 @@
 A plugin for [Bitfocus Companion](https://bitfocus.io/) to control
 [BUTT](https://danielnoethen.de/butt/) broadcasting software.
 
-**Available actions**
+### Available actions
 
+#### Streaming
 * Start streaming
 * Stop streaming
-* Toggle streaming
+* Toggle streaming (Coming very soon)
+* Set streaming signal threshold
+* Set streaming silence threshold
 
-**Feedback**
+#### Recording
+* Start recording
+* Stop recording
+* Toggle recording (Coming very soon)
+* Split recording
+* Set recording signal threshold
+* Set recording silence threshold
 
-* Streaming status
+#### Others
+* Update song name
 
-**Variables**
+### Feedback
+
+* Streaming status (Coming very soon)
+* Recording status (Coming very soon)
+* Signal status (Coming very soon)
+
+### Variables
 
 * Song title (Coming soon)
+* Streaming/recording duration (Coming soon)
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const instance_skel = require('../../instance_skel')
+const { exec } = require("child_process");
 
 class instance extends instance_skel {
 	/**
@@ -23,10 +24,6 @@ class instance extends instance_skel {
 	updateCustomVariables = (variables) => {
 		this.customVariables = variables
 		this.actions()
-	}
-
-	static GetUpgradeScripts() {
-		return []
 	}
 
 	updateConfig(config) {
@@ -83,17 +80,22 @@ class instance extends instance_skel {
 				id: 'binary_path',
 				label: 'BUTT binary path on the server (either butt or butt-client binary)',
 				width: 12,
+				required: true
 			},
 			{
 				type: 'textinput',
 				id: 'server_ip',
 				label: 'IP address of the server (default: 127.0.0.1)',
+				default: '127.0.0.1',
 				width: 12,
 			},
 			{
-				type: 'textinput',
+				type: 'number',
 				id: 'server_port',
 				label: 'Port of the server (default: 1256)',
+				min: 1,
+				max: 65535,
+				default: 1256,
 				width: 12,
 			}
 		]
@@ -105,172 +107,99 @@ class instance extends instance_skel {
 		this.system.removeListener('custom_variables_update', this.updateCustomVariables)
 	}
 
-	FIELD_URL = {
-		type: 'textwithvariables',
-		label: 'URL',
-		id: 'url',
-		default: '',
+	FIELD_THRESHOLD = {
+		type: 'number',
+		label: 'Seconds',
+		id: 'threshold',
+		min: 0,
+		max: 1000,
+		required: true
 	}
-
-	FIELD_BODY = {
-		type: 'textwithvariables',
-		label: 'Body',
-		id: 'body',
-		default: '{}',
-	}
-
-	FIELD_HEADER = {
-		type: 'textwithvariables',
-		label: 'header input(JSON)',
-		id: 'header',
-		default: '',
-	}
-
-	FIELD_CONTENTTYPE = {
-		type: 'dropdown',
-		label: 'Content Type',
-		id: 'contenttype',
-		default: 'application/json',
-		choices: [
-			{ id: 'application/json', label: 'application/json' },
-			{ id: 'application/x-www-form-urlencoded', label: 'application/x-www-form-urlencoded' },
-			{ id: 'application/xml', label: 'application/xml' },
-			{ id: 'text/html', label: 'text/html' },
-			{ id: 'text/plain', label: 'text/plain' },
-		],
-	}
-
-	FIELD_JSON_DATA_VARIABLE = null
 
 	actions() {
-		this.FIELD_JSON_DATA_VARIABLE = {
-			type: 'dropdown',
-			label: 'JSON Response Data Variable',
-			id: 'jsonResultDataVariable',
-			default: '',
-			choices: Object.entries(this.customVariables).map(([id, info]) => ({
-				id: id,
-				label: id,
-			})),
-		}
-		this.FIELD_JSON_DATA_VARIABLE.choices.unshift({ id: '', label: '<NONE>' })
-
 		this.setActions({
-			post: {
-				label: 'POST',
-				options: [this.FIELD_URL, this.FIELD_BODY, this.FIELD_HEADER, this.FIELD_CONTENTTYPE],
+			start_streaming: {
+				label: 'Start streaming'
 			},
-			get: {
-				label: 'GET',
-				options: [this.FIELD_URL, this.FIELD_HEADER, this.FIELD_JSON_DATA_VARIABLE],
+			stop_streaming: {
+				label: 'Stop streaming'
 			},
-			put: {
-				label: 'PUT',
-				options: [this.FIELD_URL, this.FIELD_BODY, this.FIELD_HEADER, this.FIELD_CONTENTTYPE],
+			toggle_streaming: {
+				label: 'Toggle streaming'
 			},
-			patch: {
-				label: 'PATCH',
-				options: [this.FIELD_URL, this.FIELD_BODY, this.FIELD_HEADER, this.FIELD_CONTENTTYPE],
+			start_recording: {
+				label: 'Start recording'
 			},
-			delete: {
-				label: 'DELETE',
-				options: [this.FIELD_URL, this.FIELD_BODY, this.FIELD_HEADER],
+			stop_recording: {
+				label: 'Stop recording'
 			},
+			toggle_recording: {
+				label: 'Toggle recording'
+			},
+			split_recording: {
+				label: 'Split recording'
+			},
+			set_streaming_signal_threshold: {
+				label: 'Set streaming signal threshold',
+				options: [this.FIELD_THRESHOLD],
+			},
+			set_streaming_silence_threshold: {
+				label: 'Set streaming silence threshold',
+				options: [this.FIELD_THRESHOLD],
+			},
+			set_recording_signal_threshold: {
+				label: 'Set recording signal threshold',
+				options: [this.FIELD_THRESHOLD],
+			},
+			set_recording_silence_threshold: {
+				label: 'Set recording silence threshold',
+				options: [this.FIELD_THRESHOLD],
+			},
+			update_song_name: {
+				label: 'Update song name',
+				options: [
+					{
+						type: 'textinput',
+						label: 'Song name',
+						id: 'song_name',
+						required: true
+					}
+				]
+			}
 		})
+	}
+
+	invoke_binary = (args) => {
+		let cmd = `${this.config.binary_path} -a ${this.config.server_ip} -p ${this.config.server_port} ${args.join(' ')}`
+		this.debug('invoke_binary', cmd)
+
+		exec(cmd, (error, stdout, stderr) => {
+			if (error) {
+				this.log('error', `exec error: ${error}, sdterr: ${stderr}, stdout: ${stdout}`)
+				return;
+			}
+			this.log('info', `exec success, stdout: ${stdout}`)
+		});
 	}
 
 	action(action) {
-		let cmd = ''
-		let body = {}
-		let header = {}
-		let restCmds = {
-			post: 'rest',
-			get: 'rest_get',
-			put: 'rest_put',
-			patch: 'rest_patch',
-			delete: 'rest_delete',
+		let args = {
+			start_streaming: ['-s'],
+			stop_streaming: ['-d'],
+			start_recording: ['-r'],
+			stop_recording: ['-t'],
+			split_recording: ['-n'],
+			set_streaming_signal_threshold: ['-M', action.options.threshold],
+			set_streaming_silence_threshold: ['-m', action.options.threshold],
+			set_recording_signal_threshold: ['-O', action.options.threshold],
+			set_recording_silence_threshold: ['-o', action.options.threshold],
+			update_song_name: ['-u', `"${action.options.song_name}"`],
+			
 		}
-		let restCmd = restCmds[action.action]
-		let errorHandler = (e, result) => {
-			if (e !== null) {
-				this.log('error', `HTTP ${action.action.toUpperCase()} Request failed (${e.message})`)
-				this.status(this.STATUS_ERROR, result.error.code)
-			} else {
-				this.status(this.STATUS_OK)
-			}
-		}
+		// TODO toggle streaming and recording by querying status first
+		// toggle_streaming, toggle_recording
 
-		let jsonResultDataHandler = (e, result) => {
-			if (e !== null) {
-				this.log('error', `HTTP ${action.action.toUpperCase()} Request failed (${e.message})`)
-				this.status(this.STATUS_ERROR, result.error.code)
-			} else {
-				// store json result data into retrieved dedicated custom variable
-				let jsonResultDataVariable = action.options.jsonResultDataVariable
-				if (jsonResultDataVariable !== '') {
-					this.debug('jsonResultDataVariable', jsonResultDataVariable)
-					let jsonResultData = JSON.stringify(result.data)
-					this.system.emit('custom_variable_set_value', jsonResultDataVariable, jsonResultData)
-				}
-				this.status(this.STATUS_OK)
-			}
-		}
-
-		let options = {
-			connection: {
-				rejectUnauthorized: this.config.rejectUnauthorized,
-			},
-		}
-
-		this.system.emit('variable_parse', action.options.url, (value) => {
-			cmd = value
-		})
-
-		if (action.options.url.substring(0, 4) !== 'http') {
-			if (this.config.prefix.length > 0) {
-				cmd = `${this.config.prefix}${cmd.trim()}`
-			}
-		}
-
-		if (action.options.body && action.options.body.trim() !== '') {
-			this.system.emit('variable_parse', action.options.body, (value) => {
-				body = value
-			})
-
-			if (action.options.contenttype && action.options.contenttype === 'application/json') {
-				//only parse the body if we are explicitly sending application/json
-				try {
-					body = JSON.parse(body)
-				} catch (e) {
-					this.log('error', `HTTP ${action.action.toUpperCase()} Request aborted: Malformed JSON Body (${e.message})`)
-					this.status(this.STATUS_ERROR, e.message)
-					return
-				}
-			}
-		}
-
-		if (action.options.header.trim() !== '') {
-			this.system.emit('variable_parse', action.options.header, (value) => {
-				header = value
-			})
-
-			try {
-				header = JSON.parse(header)
-			} catch (e) {
-				this.log('error', `HTTP ${action.action.toUpperCase()} Request aborted: Malformed JSON Header (${e.message})`)
-				this.status(this.STATUS_ERROR, e.message)
-				return
-			}
-		}
-
-		if (restCmd === 'rest_get') {
-			this.system.emit(restCmd, cmd, jsonResultDataHandler, header, options)
-		} else {
-			if (action.options.contenttype) {
-				header['Content-Type'] = action.options.contenttype
-			}
-			this.system.emit(restCmd, cmd, body, errorHandler, header, options)
-		}
+		this.invoke_binary(args[action.action])
 	}
 }
 exports = module.exports = instance
